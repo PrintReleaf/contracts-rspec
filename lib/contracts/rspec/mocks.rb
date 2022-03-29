a1333e0308da3fe03b11bb0eb9deec968a68d2a1
carl-printreleaf <cdileo@printreleaf.com>
2022-03-28 20:25:36 -0600

    Handle mock return value ParamContractError type check

--- lib/contracts/rspec/mocks.rb
+++ lib/contracts/rspec/mocks.rb
@@ -4,6 +4,7 @@ module Contracts
       def instance_double(klass, *args)
         super.tap do |double|
           allow(double).to receive(:is_a?).with(klass).and_return(true)
+          allow(double).to receive(:is_a?).with(ParamContractError).and_return(klass.is_a?(ParamContractError))
         end
       end
     end
